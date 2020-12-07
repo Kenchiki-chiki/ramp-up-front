40aead24cd02444513715e8d337dd19ea05b3bfe
Kenchiki-chiki <kodama303@gmail.com>
2020-12-07 14:12:20 +0900

    フラッシュメッセージ機能の追加

--- pages/skill.vue
+++ pages/skill.vue
@@ -1,3 +1,89 @@
 <template>
-  <div>スキルを設定しましょう</div>
+  <v-container>
+    <v-card width="400px" class="mx-auto mt-5 skill-form">
+      <v-card-title>
+        <h1 class="display-1">
+          スキルを設定しましょう。
+        </h1>
+      </v-card-title>
+      <v-card-text>
+        <v-form ref="form" lazy-validation>
+
+          <v-row>
+
+        <v-col
+          cols="4"
+        >
+          <v-text-field
+            label="スキル 1"
+            placeholder="(例)Vue"
+            filled
+          ></v-text-field>
+        </v-col>
+        <v-col
+          cols="4"
+        >
+          <v-text-field
+            label="スキル 2"
+            placeholder="(例)React"
+            filled
+          ></v-text-field>
+        </v-col>
+        <v-col
+          cols="4"
+        >
+          <v-text-field
+            label="スキル 3"
+            placeholder="(例)Angular"
+            filled
+          ></v-text-field>
+        </v-col>
+        <v-col
+          cols="4"
+        >
+          <v-text-field
+            label="スキル 4"
+            placeholder="(例)Rails"
+            filled
+          ></v-text-field>
+        </v-col>
+        <v-col
+          cols="4"
+        >
+          <v-text-field
+            label="スキル 5"
+            placeholder="(例)AWS"
+            filled
+          ></v-text-field>
+        </v-col>
+        <v-col
+          cols="4"
+        >
+          <v-text-field
+            label="スキル 6"
+            placeholder="(例)英語"
+            filled
+          ></v-text-field>
+        </v-col>
+          </v-row>
+          
+          
+          
+          <v-card-actions>
+            <v-btn
+              color="#666666"
+              class="white--text"
+            >
+              設定完了
+            </v-btn>
+          </v-card-actions>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </v-container>
 </template>
+
+
+<style>
+
+</style>
--- store/message.js
+++ store/message.js
@@ -16,13 +16,19 @@ const state = {
 // 受け取った引数contentをstateのcontentに代入
 const mutations = {
   setContent (state, { content, timeout }) {
+    console.log('======1======')
     state.content = content
+    console.log('======2======')
 
     if (typeof timeout === 'undefined') {
+      console.log('======3======')
       timeout = 3000
+      console.log('======4======')
     }
+    console.log('======5======')
 
     setTimeout(() => (state.content = ''), timeout)
+    console.log('======6======')
   }
 }
 
