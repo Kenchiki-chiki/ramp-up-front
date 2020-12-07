25fc7cd232c5676abcce37e3591599d4676ed5a0
Kenchiki-chiki <kodama303@gmail.com>
2020-12-07 16:45:20 +0900

    スキル設定機能の追加

--- pages/skill.vue
+++ pages/skill.vue
@@ -1,3 +1,113 @@
 <template>
-  <div>スキルを設定しましょう</div>
+  <v-container>
+    <v-card width="400px" class="mx-auto mt-5 skill-form">
+      <v-card-title>
+        <h1 class="display-1">
+          スキルを設定しましょう。
+        </h1>
+      </v-card-title>
+      <v-card-text>
+        <v-form ref="form" lazy-validation>
+
+          <v-row>
+
+        <v-col
+          cols="4"
+        >
+          <v-text-field
+            v-model="skill.name"
+            label="スキル 1"
+            placeholder="(例)Vue"
+            filled
+          ></v-text-field>
+        </v-col>
+        <v-col
+          cols="4"
+        >
+          <v-text-field
+            v-model="skill.name"
+            label="スキル 2"
+            placeholder="(例)React"
+            filled
+          ></v-text-field>
+        </v-col>
+        <v-col
+          cols="4"
+        >
+          <v-text-field
+            v-model="skill.name"
+            label="スキル 3"
+            placeholder="(例)Angular"
+            filled
+          ></v-text-field>
+        </v-col>
+        <v-col
+          cols="4"
+        >
+          <v-text-field
+            v-model="skill.name"
+            label="スキル 4"
+            placeholder="(例)Rails"
+            filled
+          ></v-text-field>
+        </v-col>
+        <v-col
+          cols="4"
+        >
+          <v-text-field
+            v-model="skill.name"
+            label="スキル 5"
+            placeholder="(例)AWS"
+            filled
+          ></v-text-field>
+        </v-col>
+        <v-col
+          cols="4"
+        >
+          <v-text-field
+            v-model="skill.name"
+            label="スキル 6"
+            placeholder="(例)英語"
+            filled
+          ></v-text-field>
+        </v-col>
+          </v-row>
+          
+          
+          
+          <v-card-actions>
+            <v-btn
+              @click="onSubmit"
+              color="#666666"
+              class="white--text"
+            >
+              設定完了
+            </v-btn>
+          </v-card-actions>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </v-container>
 </template>
+
+<script>
+export default {
+  data() {
+    return{
+      skill: {}
+    }
+  },
+  methods: {
+    async onSubmit() {
+      await this.$store.dispatch('addSkill', this.skill)
+      this.skill.name = ''
+    }
+  }
+
+}
+</script>
+
+
+<style>
+
+</style>
