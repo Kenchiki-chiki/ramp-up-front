7358b0e3604399896cb1bf0a8c5f0b17ac5e9e58
Kenchiki-chiki <kodama303@gmail.com>
2020-12-24 18:24:45 +0900

    本番環境のエンドポイントにも対応するように修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,11 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
-  
+  // 本番環境と開発環境それぞれのエンドポイントに対応
+  axios: {
+    baseURL: process.env.NODE_ENV === "production" ? "https://ramp-up-api.herokuapp.com" : "http://localhost:8080"
+
+  },
   
   // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
   ssr: false,
@@ -88,9 +92,5 @@ export default {
   build: {
   },
 
-  // 本番環境と開発環境それぞれのエンドポイントに対応
-  axios: {
-        baseURL: process.env.NODE_ENV === "production" ? "https://ramp-up-api.herokuapp.com" : "http://localhost:8080"
-   
-      }
+  
 }
